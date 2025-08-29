b60c5ebf1bc09316fc7508afeaa4cf27dd3f250b
codearemos-online <dev@codearemos.online>
2025-08-28 21:51:56 -0500

    feat(list-rendering) Implement sizes and variants

--- main.js
+++ main.js
@@ -10,6 +10,25 @@ const app = Vue.createApp({
                 "50% cotton",
                 "30% wool",
                 "20% polyester"
+            ],
+            variants:[
+                {
+                    id:2234,
+                    color:`green`,
+                    image:`./assets/images/socks_green.jpg`,
+                },
+                {
+                    id:2235,
+                    color:`blue`,
+                    image:`./assets/images/socks_blue.jpg`,
+                },
+            ],
+            sizes:[
+                "S",
+                "M",
+                "L",
+                "XL",
+                "XXL"
             ]
         }
     }
